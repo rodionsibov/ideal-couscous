58a3608edcc6d5b0af4354c1d175574d0f6d3040
Rodion Sibov <rodionsibov@gmail.com>
2022-02-08 21:52:11 +0200

    update

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,8 +1,31 @@
 <script setup>
-
 </script>
 
 <template>
-<h1>Vuex Counter</h1>
-{{$store.state.name}}
+  <h1>Vuex Counter</h1>
+  <h2 class="counter">0</h2>
+  <button>-</button>
+  <input type="number" />
+  <button>+</button>
 </template>
+
+<style scoped>
+.counter {
+  font-size: 5rem;
+}
+
+button {
+    border-radius: 100%;
+    border: none;
+    width: 2rem;
+    height: 2rem;
+    font-weight: 700;
+    cursor: pointer;
+}
+
+input {
+    padding: 0.4rem;
+    margin: 0 0.5rem;
+    text-align: center;
+}
+</style>
