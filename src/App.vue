0eb6347e822d3ce8c61ebf90bc1525c60d7d4b1d
Rodion Sibov <rodionsibov@gmail.com>
2022-02-08 19:03:41 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,17 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import Component1 from './components/Component1.vue'
+import Component2 from './components/Component2.vue'
+import Component3 from './components/Component3.vue'
+import Component4 from './components/Component4.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <Component1 />
+  <Component2 />
+  <Component3 />
+  <Component4 />
 </template>
 
 <style>
