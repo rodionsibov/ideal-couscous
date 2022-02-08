d0976db966329569c340de6084e0e75fe1d658f3
Rodion Sibov <rodionsibov@gmail.com>
2022-02-08 19:02:31 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Component1 from './components/Component1.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Component1 />
 </template>
 
 <style>
