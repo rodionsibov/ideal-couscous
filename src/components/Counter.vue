83c4c9cd5ae0e57336248dcabd1ee84516f88dfd
Rodion Sibov <rodionsibov@gmail.com>
2022-02-11 20:18:47 +0200

    update

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -5,7 +5,12 @@ import { useStore } from "vuex";
 const store = useStore();
 const value = ref(0);
 
+const setCustomValidity = ref(null);
+
 onMounted(() => {
+  setTimeout(() => {
+    setCustomValidity.value.focus()
+  }, 2000);
   store.dispatch("generateData");
 });
 
@@ -37,6 +42,7 @@ const addRandomNumber = () => store.dispatch("addRandomNumber");
 </div> -->
 
   <div v-if="!store.state.isLoading">
+    <input ref="setCustomValidity" type="text" value="set custom validity" />
     <h1>Vuex Counter</h1>
     <h2 class="counter">
       {{ $store.state.counter }}
@@ -44,7 +50,23 @@ const addRandomNumber = () => store.dispatch("addRandomNumber");
     <button @click="subtractFromCounter(value)">-</button>
     <input type="number" v-model="value" />
     <button @click="addToCounter(value)">+</button>
-    <div class="btn" @click="addRandomNumber">Add Random Number</div>
+    <div class="btn" @click="addRandomNumber">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      Add by Random Number
+    </div>
   </div>
   <div v-else>Data is loading...</div>
 </template>
@@ -71,16 +93,17 @@ input {
 }
 
 .btn {
+  display: flex;
+  align-items: center;
   cursor: pointer;
   font-weight: bold;
   text-transform: uppercase;
-  letter-spacing: 1px;
   font-size: 0.8rem;
   margin: 30px auto;
   width: fit-content;
   padding: 16px 24px;
   border-radius: 50px;
-  background-color: #00A36C;
+  background-color: #00a36c;
   color: white;
   box-shadow: 2px 2px 4px rgba(0, 128, 128, 0.8);
   text-shadow: 1px 1px 1px teal;
@@ -88,11 +111,16 @@ input {
 }
 
 .btn:hover {
-  transform: translateY(-2px);
-  box-shadow: 2px 2px 16px rgba(0, 128, 128, 0.8)
+  box-shadow: 2px 2px 16px rgba(0, 128, 128, 0.8);
+  letter-spacing: 1px;
 }
 
 .btn:active {
   background-color: teal;
+}
+
+.btn svg {
+  width: 20px;
+  margin-right: 6px;
 }
 </style>
